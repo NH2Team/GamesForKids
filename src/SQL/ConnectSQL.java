f3ebd33c313d693a72e1213de002135e71cee401
Nguyen Huu Nhan <nguyennhan.tg2@gmail.com>
2021-03-10 20:35:39 +0700

    fix file SQL ConnectionSQL

--- src/SQL/ConnectSQL.java
+++ src/SQL/ConnectSQL.java
@@ -16,26 +16,25 @@ import java.sql.Statement;
  * @author nhand
  */
 public class ConnectSQL{
-//    final String DB_URL = "jdbc:sqlserver://localhost;databaseName=USER;user=sa;password=sa";
-//    Connection conn = DriverManager.getConnection(DB_URL);
-//    Statement stmt = conn.createStatement();
-//
-//    public int updateData(String sql) throws SQLException {
-//        conn = DriverManager.getConnection(DB_URL);
-//        stmt = conn.createStatement();
-//        return stmt.executeUpdate(sql);
-//    }
-//    
-//    public ResultSet getData(String sql) throws SQLException{
-//        return stmt.executeQuery(sql);
-//    }
-//    
-//    
+    final String DB_URL = "jdbc:sqlserver://localhost;databaseName=USER;user=sa;password=sa";
+    Connection conn = null;
+    Statement stmt = null;
+
+    public int updateData(String sql) throws SQLException {
+        conn = DriverManager.getConnection(DB_URL);
+        stmt = conn.createStatement();
+        return stmt.executeUpdate(sql);
+    }
+    
+    public ResultSet getData(String sql) throws SQLException{
+        return stmt.executeQuery(sql);
+    }
+      
     public static void main(String[] args) throws SQLException{
         ConnectSQL csql = new ConnectSQL();
         String sql = "INSERT INTO ACCOUNT VALUES('hayko','hayko')";
         System.out.println(sql);
-        //System.out.println(csql.updateData(sql));
+        System.out.println(csql.updateData(sql));
         //Dang Test
     }
 }
